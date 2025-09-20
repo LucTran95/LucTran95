2fdbccc2c538f93bbe4a4bb2f518d1f19b73a5c5
LucTran95 <huuluc95@gmail.com>
2025-09-20 20:35:10 +0700

    Create bai1.js

---
+++ Bai1/bai1.js
@@ -0,0 +1,20 @@
+alert('xin chao cac ban tre')
+
+console.log('Minh la javascript')
+
+let studentname = 'Luc'
+console.log(studentname)
+
+
+let diemso = 9
+console.log (diemso)
+
+diemso = 11
+console.log(diemso)
+
+
+const PI = 3.14
+console.log(PI)
+
+PI=3
+console.log(PI)
